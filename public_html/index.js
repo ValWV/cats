7f9b9e3388635f80aca6e134409b020ab1f37bb2
Val <33302737+ValWV@users.noreply.github.com>
2018-11-16 19:28:31 +0300

    Update index.js

--- public_html/index.js
+++ public_html/index.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
-var http = require('http'); // Необходимый модуль
+var http = require('http'); 
 
-// Создание сервера
+
 var server = http.createServer(function(req, res) {
-  // Указание заголовков (тип данных и кодировка)
+  
   console.log("URL страницы: + req.url");
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   var myReadShort = fs.createReadStream(__dirname + '/cats1.html', 'utf-8');
   myReadShort.pipe(res);
 });
-// server - переменная созданная ранее
+
 server.listen(3000, '127.0.0.1');
